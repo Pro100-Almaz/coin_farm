0486bd6486d4d35fb4bf3a2bf0d93329aa00f2bf
almaz amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-07-04 15:17:32 +0500

    added: hash logic

---
+++ app/utils.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from typing import Union
+from dotenv import load_dotenv
+from passlib.context import CryptContext
+
+import os
+import jwt
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_password(plain_password, hashed_password):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    return pwd_context.hash(password)
